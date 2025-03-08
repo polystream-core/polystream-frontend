bf1d9a69758de3ac0ea20371f0663995472fff68
yangding14 <122850418+yangding14@users.noreply.github.com>
2025-03-08 19:24:37 +0800

    add tabs

---
+++ app/(tabs)/portfolio/_layout.tsx
@@ -0,0 +1,25 @@
+import * as SplashScreen from 'expo-splash-screen';
+import { Redirect, Stack } from "expo-router";
+import { usePrivy } from '@privy-io/expo';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function Layout() {
+
+    const {user} = usePrivy();
+
+    if (!user) {
+        return <Redirect href={"/sign-in"} />;
+    }
+
+    return (
+            <Stack
+          screenOptions={{
+            headerShown: false
+          }}
+          >
+            <Stack.Screen
+            name='polystream-vault' />
+          </Stack>
+    );
+}
