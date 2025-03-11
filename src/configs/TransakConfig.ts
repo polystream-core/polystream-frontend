2fea8335dbe5aee46bf0a9d7869670399e87a3f8
yangding14 <122850418+yangding14@users.noreply.github.com>
2025-03-11 15:10:35 +0800

    dynamic address wallet for transak

--- src/configs/TransakConfig.ts
+++ src/configs/TransakConfig.ts
@@ -1,16 +1,20 @@
 import { env } from "@/src/constants/AppConfig";
 import { Environments, TransakConfig } from "@transak/react-native-sdk";
-import {colors} from "@/src/constants/Colors";
+import { colors } from "@/src/constants/Colors";
+import { uuidv7 } from "uuidv7";
 
-export const transakConfig = (configOverrides: Record<string, any> = {}): TransakConfig => ({
-  apiKey: env.TRANSAK_API_KEY,
-  environment: Environments.STAGING,
-  partnerOrderId: "order-12345",
-  walletAddress: "0x1375a0818014C96D1C0B723946c4835BFe4a32F5",
-  fiatCurrency: "USD",
-  defaultCryptoCurrency: "USDC",
-  themeColor: colors.beige.primary,
-  exchangeScreenTitle: "PolyStream - Top Up",
-  email: "",
-  ...configOverrides
-});
+export function createTransakConfig(walletAddress : string, configOverrides: Record<string, any> = {}): TransakConfig {
+  return {
+    apiKey: env.TRANSAK_API_KEY,
+    environment: Environments.STAGING,
+    partnerOrderId: uuidv7(), // Generate unique order ID using uuidv7
+    walletAddress: walletAddress,
+    fiatCurrency: "USD",
+    defaultCryptoCurrency: "USDC",
+    themeColor: colors.beige.color01,
+    exchangeScreenTitle: "PolyStream - Top Up",
+    // disableWalletAddressForm: true,
+    email: "",
+    ...configOverrides
+  };
+}
